eb44a81bb938988cc7dba539f2aed02445a5d9bd
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-02-29 08:52:15 -0300

    att

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-let total = document.getElementsByClassName("card-text");
-console.log(total);
+var corAtual = "preto";
 
-let isLightMode = true;
-
-document.getElementById("modo").addEventListener("click", function() {
-    if (document.body.style.backgroundColor === "black") {
-        document.body.style.backgroundColor = "white";
-        document.getElementById("modo").textContent = "Alternar para Modo Escuro";
-        // Mudar a cor de texto para preto nos elementos <p> e <h1>
-        document.querySelectorAll('p, h1').forEach(element => {
-            element.style.color = "black";
-        });
-    }else {
+function mudarCorDeFundo() {
+    if (corAtual === "preto") {
+        document.body.style.backgroundColor = "gray";
+        corAtual = "cinza";
+    } else {
         document.body.style.backgroundColor = "black";
-        document.getElementById("modo").textContent = "Alternar para Modo Claro";
-        //mudar a cor do texto para branco nos elementos <p> e <h1>
-        document.querySelectorAll('h1').forEach(element => {
-
-        });
+        corAtual = "preto";
     }
-    //aternar a variável para a próxima cor
-    isLightMode = !isLightMode;
-});
+}
