bc29a8d7edc87972d7201905cd3ea0658a9892c6
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-03-05 10:22:51 -0300

    Atualização que deu errado

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+document.getElementById("meuP").textContent = "novo conteudo";
+
+document.getElementById("ola").style.backgroundColor = "red";
+
+
+// Mudar cor de fundo da página
 var corAtual = "preto";
 
 function mudarCorDeFundo() {
@@ -9,3 +15,30 @@ function mudarCorDeFundo() {
         corAtual = "preto";
     }
 }
+
+
+
+// Teste
+let titulo = document.getElementById("titulo");
+console.log(titulo);
+
+let isLightMode = true;
+
+document.getElementById("modo").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        document.getElementById("modo").textContent = "Alternar para Modo Claro";
+        document.querySelectorAll('p').forEach(element => {
+            element.style.color = "white";
+        });
+    } else {
+        document.getElementById("modo").textContent = "Alternar para Modo Escuro";
+        document.querySelectorAll('p').forEach(element => {
+            element.style.color = "black";
+        });
+    }
+
+    // You can still use the isLightMode variable if needed
+    isLightMode = !isLightMode;
+});
